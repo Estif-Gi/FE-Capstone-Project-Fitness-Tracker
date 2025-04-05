2bdb5f9416c4a54798a2ff4d88fc1e9063f7a464
Estifanos Girma <estifanosgirma4@gmail.com>
2025-04-05 12:47:22 +0300

    tailwindcc 2

--- get-fit-app/src/pages/footer.jsx
+++ get-fit-app/src/pages/footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
     return ( 
-        <footer className=" bg-gray-500 bg-opacity-65 p-4 pt-10 pb-16 md:pt-20 md:pb-20">
+        <footer className=" bg-gray-500 bg-opacity-65 p-4 pt-10 pb-16 md:pt-20 md:pb-16">
             <div className="flex justify-between item-center md:gap-11 items-center mr-6 ">
                 <Link to="/"><img className="w-28 h-fit md:w-36 "
                 src="../../image/Get Fit.png" alt="Logo"></img></Link>
@@ -12,7 +12,7 @@ const Footer = () => {
                 <a href="#"><img className="w-10 h-fit md:w-12 " src="../../image/twitter.png" alt="twitter" /></a>
             </div>
             </div>
-            <p className=" text-center flex self-end translate-y-16 md:translate-y-20 justify-self-center text-orange-50 text-sm text-opacity-40 ">Copyright © 2025 Get Fit</p>
+            <p className=" text-center flex self-end translate-y-16  justify-self-center text-orange-50 text-sm text-opacity-40 ">Copyright © 2025 Get Fit</p>
         </footer>
      );
 }
